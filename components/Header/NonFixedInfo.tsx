7e35ac04df2b9ad006b484800b3ee3510e6cb960
addey-eweis <addeyeweisofficial@gmail.com>
2023-09-24 09:25:37 +0300

    Added New Section

---
+++ components/Header/NonFixedInfo.tsx
@@ -0,0 +1,12 @@
+import styles from "@/components/Header/NonFixedInfo.module.scss";
+import React from "react";
+
+export default function NonFixedInfo(){
+    return(
+        <>
+            <section>
+                <h1>Experience</h1>
+            </section>
+        </>
+    )
+}
--- components/Homepage/HomePage.tsx
+++ components/Homepage/HomePage.tsx
@@ -1,12 +1,14 @@
 import styles from "./HomePage.module.scss"
 import React from 'react';
 import MyInfoLanding from "@/components/Header/MyInfoLanding";
+import NonFixedInfo from "@/components/Header/NonFixedInfo";
 
 export default function HomePage(){
     return(
         <>
             <section className={styles.main}>
                 <MyInfoLanding/>
+                <NonFixedInfo/>
             </section>
         </>
     )
